a845d77b5a8eeb47681697210418747d6de000c4
João Fonseca <joao.fonseca@ua.pt>
2023-11-08 18:10:02 +0000

    ES-30 Remove API Gateway for now

--- api-gateway.tf
+++ api-gateway.tf
@@ -1,12 +1,12 @@
 
-resource "aws_apigatewayv2_api" "api_gw" {
-  name          = "project-x-api-gw"
-  protocol_type = "HTTP"
-}
-
-output "api_gw_id" {
-  value = aws_apigatewayv2_api.api_gw.id
-}
-output "api_gw_endpoint" {
-  value = aws_apigatewayv2_api.api_gw.api_endpoint
-}
+# resource "aws_apigatewayv2_api" "api_gw" {
+#   name          = "project-x-api-gw"
+#   protocol_type = "HTTP"
+# }
+# 
+# output "api_gw_id" {
+#   value = aws_apigatewayv2_api.api_gw.id
+# }
+# output "api_gw_endpoint" {
+#   value = aws_apigatewayv2_api.api_gw.api_endpoint
+# }
